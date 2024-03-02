560242110ef9f1f6319f7c9a6134952cf57ddea2
Geradon-Pachenson <pacheniy@mail.ru>
2024-03-02 17:35:55 +0500

    Некорректно отрабатывает подсчет очков после рестарта

--- script/Area.js
+++ script/Area.js
@@ -1,30 +1,32 @@
 class Area {
-    constructor() {
+    constructor(cells) {
+        this.cells = cells;
     }
 
     draw() { 
         this.body = document.querySelector(".body");
         this.body.innerHTML += '<div class="area"></div>';
         this.area = document.querySelector(".area");
-        //создаем кнопку restart и делаем ее невидимой
+        //создаем кнопки start и restart. Делаем restart невидимой.
         this.area = document.querySelector(".area");
-        this.area.innerHTML = `<button type="button" class="btn__restart">restart</button>`;
+        this.area.innerHTML = `<button type="button" class="btn__start">start</button>`;
+        this.area.innerHTML += `<button type="button" class="btn__restart">restart</button>`;
         //отрисовка игрового поля
         for (let i = 1; i <= 100; i++) {
             this.area.innerHTML += '<div class="cell"></div>'
         }
     
-        this.cell = document.querySelectorAll(".cell");
+        this.cells = document.querySelectorAll(".cell");
         let x = 1;
         let y = 10;
     
-        for (let i = 0; i < this.cell.length; i++) {
+        for (let i = 0; i < this.cells.length; i++) {
             if(x > 10) {
                 x = 1;
                 y--;
             }
-            this.cell[i].setAttribute('positionX', x);
-            this.cell[i].setAttribute('positionY', y);
+            this.cells[i].setAttribute('positionX', x);
+            this.cells[i].setAttribute('positionY', y);
             x++;
         } 
     }
